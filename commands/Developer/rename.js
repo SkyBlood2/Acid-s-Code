db27c6057b02986902ecff3438ee1d76c3df746a
SkyBlood2 <110068489+SkyBlood2@users.noreply.github.com>
2022-07-26 16:48:25 -0700

    Create rename.js

---
+++ commands/Developer/rename.js
@@ -0,0 +1,8 @@
+module.exports = {
+  name: "remameBot",
+  aliases: ["nameBot"],
+  code: `$setBotName[Acid] 
+Setted my name to $message!
+$onlyForIDs[938976034395668561;987862277694816326; This command is only for my developers.]
+`   
+}
