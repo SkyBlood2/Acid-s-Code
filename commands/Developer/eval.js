03966e6996ef5b3dc32dfab37347d2ec498aea1a
SkyBlood2 <110068489+SkyBlood2@users.noreply.github.com>
2022-07-26 16:47:56 -0700

    Create eval.js

---
+++ commands/Developer/eval.js
@@ -0,0 +1,7 @@
+module.exports = {
+  nonPrefixed: false,
+  name: "eval",
+  code: `$eval[$message]
+$onlyForIDs[938976034395668561;987862277694816326; This command is only for my developers.]
+`
+}
