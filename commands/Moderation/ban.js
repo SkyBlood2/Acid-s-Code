b3021418fe38f60d38502e12ae0d86626ce5dade
SkyBlood2 <110068489+SkyBlood2@users.noreply.github.com>
2022-07-26 16:50:54 -0700

    Delete ban.js

--- commands/Moderation/ban.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  name: "ban",
-  aliases: ["bye", "kill", "banish", "exile"],
-  code: `
-$ban[$guildID;$mentioned[1;no];;$noMentionMessage]
-$username[$mentioned[1;no]] has been exiled from Project: Heaven.
-$onlyPerms[ban;You cannot use this command.]`
-  
-}
