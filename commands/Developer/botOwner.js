5639e6a8ce2357ea84cd6473470718b2a8b3f7d5
SkyBlood2 <110068489+SkyBlood2@users.noreply.github.com>
2022-07-26 16:47:02 -0700

    Create botOwner.js

---
+++ commands/Developer/botOwner.js
@@ -0,0 +1,6 @@
+module.exports = {
+  name: "BotOwner",
+  code: `My owner/s are/is <@$botOwnerID>.
+$onlyForIDs[938976034395668561;987862277694816326; This command is only for my developers.]
+`
+}
