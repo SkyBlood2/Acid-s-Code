073cf86dddd9b5ac858818208a1c2f5ef8c14052
SkyBlood2 <110068489+SkyBlood2@users.noreply.github.com>
2022-07-26 16:41:33 -0700

    Create activity.js

---
+++ commands/Developer/activity.js
@@ -0,0 +1,6 @@
+module.exports = {
+  name: "activity",
+  code: `$setStatus[music;LISTENING;idle]
+My activity status has been setted.
+$onlyForIDs[938976034395668561;987862277694816326; This command is only for my developers.]`
+}
