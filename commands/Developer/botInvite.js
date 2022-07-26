0ecaff9de790d038acf704f07dc201de4309bc95
SkyBlood2 <110068489+SkyBlood2@users.noreply.github.com>
2022-07-26 16:46:40 -0700

    Create botInvite.js

---
+++ commands/Developer/botInvite.js
@@ -0,0 +1,6 @@
+module.exports = {
+  name: "BotInvite",
+  code: `$sendDM[$getBotInvite;$authorID;no]
+$onlyForIDs[938976034395668561;987862277694816326; This command is only for my developers.]
+`
+}
